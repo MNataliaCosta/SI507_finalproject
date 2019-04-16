e6c4fd7dd85cc0a9f99c052b0e0d633f3d495fc7
Natalia Costa <m.nataliascosta@gmail.com>
2019-04-15 22:18:09 -0400

    iTunes API definition

--- SI507project_tools.py
+++ SI507project_tools.py
@@ -67,14 +67,39 @@ def get_ticketmaster_music_events():
         return ticketmaster_obj
 
 ##iTunes songs
+def get_itunes_songs(artist_result):
+    iTunes_baseurl = "https://itunes.apple.com/search"
+    iTunes_d_params = {}
+    iTunes_d_params["term"] = artist_result #required param
+    iTunes_d_params["media"] = "music" #optional param
+    iTunes_d_params["entity"] = "song" #optional param
+    # iTunes_d_params["limit"] = "20" #optional param, default = 50
+    unique_identifier = params_unique_combination(iTunes_baseurl, iTunes_d_params)
+
+    if unique_identifier in CACHE_DICTION:
+        return CACHE_DICTION[unique_identifier]
+    else:
+        iTunes_resp = requests.get(iTunes_baseurl, params = iTunes_d_params)
+        iTunes_obj = json.loads(iTunes_resp.text)
+        cache_file_obj = open(CACHE_FNAME, 'w')
+        CACHE_DICTION[unique_identifier] = iTunes_obj
+        cache_file_obj.write(json.dumps(CACHE_DICTION))
+        cache_file_obj.close()
+        return iTunes_obj
 
 
 #GET DATA FROM APIs
 raw_events_data = get_ticketmaster_music_events()
 events_list = []
 
-for data in raw_events_data["_embedded"]["events"]: #create instances for my classes and append them to list? or commit them directly to database?
-    print(data)
+for events_data in raw_events_data["_embedded"]["events"]: #create instances for my classes and append them to list? or commit them directly to database?
+    print(events_data)
+
+raw_songs_data = get_itunes_songs("Ariana Grande")
+songs_list = []
+
+for songs_data in raw_songs_data["results"]:
+    print(songs_data)
 
 
 #SETUP DATABASE -- consider separating into new file
